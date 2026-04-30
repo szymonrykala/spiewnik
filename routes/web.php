b4867300b8447b67b87cffd25438b6afd4b5d8c2
Szymon Rykała <szymonrykala@gmail.com>
2026-04-30 19:53:24 +0200

    feat(performcances): setup subpages

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 // Route::middleware('auth')->group([], function() {
-Route::group([], function() {
+Route::group([], function () {
     Route::livewire('/', 'pages::main');
-    Route::livewire('/songs/search', 'pages::songs.search');
-    Route::livewire('/songs/create', 'pages::songs.view');
-    Route::livewire('/songs/{song}', 'pages::songs.view');
+
+    Route::prefix('/songs')->group(function () {
+        Route::livewire('/', 'pages::songs.search');
+        Route::livewire('/create', 'pages::songs.view');
+        Route::livewire('/{song}', 'pages::songs.view');
+    });
+
+    Route::prefix('/performances')->group(function () {
+        Route::livewire('/', 'pages::performances.search');
+        Route::livewire('/{performance}', 'pages::performances.view');
+    });
+
 });
 
 Route::livewire("/login", 'pages::login');
